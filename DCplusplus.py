10720f138e70b8728fb1a713875de5f41be0f7d8
Zoraiz Qureshi <21100130@lums.edu.pk>
2019-04-14 18:59:45 +0500

    Fixed hashing function, added hash test

--- DCplusplus.py
+++ DCplusplus.py
@@ -1,10 +1,10 @@
 import sys, getopt
 import socket
-
+import hashlib
 
 #0..2^m-1
-m = 1 # if m = 1, keyspace is 0,1
-keyspace_max = 2^m-1 
+m = 7 # if m = 7, keyspace is 0 to 127, size = 128 (2^m)
+keyspace = 2**m
 '''
 def recv_packet(c_sock):
     testdata = c_sock.recv(1)
@@ -21,28 +21,35 @@ def recv_packet(c_sock):
     return packet
 '''
 
-#each file is stored as the value in a key,value pair at the destined node
-def fileHash(filename):
-	# temporary file naming convention is "filen.txt", where n = file number, 0,1,2..
-	key = stringHasher(filename)
-	return key # m bit unsigned integer
-
+# each file is stored as the value in a key,value pair at the destined node
+# temporary file naming convention is "filen.txt", where n = file number, 0,1,2..
 # used for hashing both node names and file names
 def stringHasher(s):
-	return name
+	s_hash = hashlib.sha1(s.encode()) # encoding string to bytes
+	key = int(s_hash.hexdigest(),16) % keyspace # converting hexadecimal digest to decimal then taking % with keyspace to only keep max hash value then loop around ring
+	# for compression across the ring, extra bits leading from m are truncated (converting to leading 0s actually)
+	return key # m bit unsigned integer (only m bits are significant leftmost)
+
+def test_hash():
+	x = input("Enter string to hash (q to exit): ")
+	while(x != "q"):
+		print(stringHasher(x))
+		x = input("Enter string to hash: ")
 
 # there must be a Node class, of which an instance is created in the main
 class Node():
 	def __init__(self):
 		self.ip = "192.168.0.101"
 		self.port = 5100
-		self.name = ip+':'+port # distinct name
-		self.hashedName = stringHasher(s=name) # hash this name
-		self.id = 0
+		self.name = self.ip+str(self.port) # distinct name
+		self.id = stringHasher(self.name) # hash this name
 		# every node itself also participates as a key store
 		self.keystorage = {} # key, file_name pairs for every file stored at this node
 		self.finger_table = {} # for every node there will be a finger table as a dictionary of successors
 		print("Node object created.")
+
+	def get_id(self):
+		return self.id
 
 	def send_file(self): # parameter for who to send to
 		# header will have the packet length of the data being sent
@@ -87,6 +94,9 @@ def main(argv):
 	#script for every node, main thing, whenever a node script is run, it goes online, connects to the network
 	print("Instantiating node.")
 	new_node = Node()
+	print(f"New node's ID is {new_node.get_id()}")
+	test_hash()
+
 	new_node.join()
 	new_node.send_file()
 	new_node.leave()
